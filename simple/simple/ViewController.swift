a6314f031cc491c2f302b067476e5de12398a599
andre <andreconghau@gmail.com>
2020-10-05 14:58:03 +0700

    Image UI and fix bypass transpot security

--- simple/simple/ViewController.swift
+++ simple/simple/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var emailTxt: UITextField!
     
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,22 +25,41 @@ class ViewController: UIViewController {
         self.lbl.text = "Home Loading"
         
         // Re-render button
-        loginButton.backgroundColor = .clear
-        loginButton.layer.cornerRadius = 5
-        loginButton.layer.borderWidth = 1
+        self.loginButton.backgroundColor = .clear
+        self.loginButton.layer.cornerRadius = 5
+        self.loginButton.layer.borderWidth = 1
         
-        emailTxt.clearButtonMode = .whileEditing
+        self.emailTxt.clearButtonMode = .whileEditing
+
     }
     
     // Login Action
     @IBAction func loginAction(_ sender: Any) {
         print("Click to Login button")
         self.lbl.text = "Clicked in Login"
+        self.avatarImage.image = UIImage(named: "img1.jpg")
     }
     
     @IBAction func signUpAction(_ sender: Any) {
         print("Click to Sign Up button")
         self.lbl.text = "Clicked in Sign Up"
+        
+        // URL
+        /*
+        let imgUrl = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/505px-Apple_logo_black.svg.png")
+        */
+        let imgUrl = URL(string: "http://fotomicamesh.com/sites/default/files/styles/style_anh_lon_home/public/album/1_2.jpg?itok=Oajs8qbd")
+        
+        // DATA
+        do {
+            let imgData = try Data(contentsOf: imgUrl!)
+            // Render
+            avatarImage.image = UIImage(data: imgData)
+        } catch {
+            print("Can not load Image")
+        }
+        
+  
     }
     
 
