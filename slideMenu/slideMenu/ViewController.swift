787b39de5497fd597b5d375049cb86893d878f6e
andre <andreconghau@gmail.com>
2020-10-08 15:43:09 +0700

    sidemenu with navigation

--- slideMenu/slideMenu/ViewController.swift
+++ slideMenu/slideMenu/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        sideMenu.leftSide = true
+        // sideMenu.leftSide = true
         SideMenuManager.default.leftMenuNavigationController = sideMenu
         SideMenuManager.default.addPanGestureToPresent(toView: view)
     }
@@ -27,52 +27,4 @@ class ViewController: UIViewController  {
     
 }
 
-class MenuController: UITableViewController {
-    
-    private var menuItems: [String]
-    
-    init(with menuItems: [String]) {
-        self.menuItems = menuItems
-        super.init(nibName: nil, bundle: nil)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ITEMS")
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return menuItems.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ITEMS", for: indexPath)
-        cell.textLabel?.text = menuItems[indexPath.row]
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        // Replay to delegate about item slection
-        // StoryBoard.
-        let mainSB = UIStoryboard(name: "Main", bundle: nil)
-        var defaultScreen = "screen-1"
-        
-        
-        switch indexPath.row {
-        case 0:
-            defaultScreen = "screen-1"
-        case 1:
-            defaultScreen = "screen-2"
-        case 2:
-            defaultScreen = "screen-3"
-        case 4:
-            defaultScreen = "screen-4"
-        default:
-            defaultScreen = "screen-1"
-        }
-        // Tao ViewController Sence
-        let homeSence = mainSB.instantiateViewController(identifier: defaultScreen) as! HomeViewController
-        self.navigationController?.pushViewController(homeSence, animated: true)
-    }
-}
+
---
+++ slideMenu/slideMenu/controller/table/MenuController.swift
@@ -0,0 +1,126 @@
+//
+//  MenuController.swift
+//  slideMenu
+//
+//  Created by andre on 10/8/20.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    private var menuItems: [String]
+    
+    init(with menuItems: [String]) {
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ITEMS")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ITEMS", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Replay to delegate about item slection
+        // StoryBoard.
+        let mainSB = UIStoryboard(name: "Main", bundle: nil)
+        var defaultScreen = "screen-1"
+        
+        
+        switch indexPath.row {
+        case 0:
+            defaultScreen = "screen-1"
+        case 1:
+            defaultScreen = "screen-2"
+        case 2:
+            defaultScreen = "screen-3"
+        case 4:
+            defaultScreen = "screen-4"
+        default:
+            defaultScreen = "screen-1"
+        }
+        // Tao ViewController Sence
+        let homeSence = mainSB.instantiateViewController(identifier: defaultScreen) as! HomeViewController
+        self.navigationController?.pushViewController(homeSence, animated: true)
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
