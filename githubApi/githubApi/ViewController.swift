7aab49e435e675a8e3af649190c19d337a485ddf
andre <andreconghau@gmail.com>
2020-10-13 15:58:50 +0700

    alamofire

--- githubApi/githubApi/ViewController.swift
+++ githubApi/githubApi/ViewController.swift
@@ -8,15 +8,35 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableUsers: UITableView!
+    
+    var result:GitHubUser?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // self.alamofireRequest()
+        self.tableUsers.dataSource = self
         
-        self.alamofireRequestJson()
+        self.alamofireRequestJson { (result, error) in
+            print(result,error)
+
+            self.result = result as? GitHubUser
+            self.tableUsers.reloadData()
+        }
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! UserTableViewCell
+        cell.name.text = result?.items![indexPath.row].login
+        return cell
     }
     
     func alamofireRequest () {
@@ -25,12 +45,12 @@ class ViewController: UIViewController {
         }
     }
     
-    func alamofireRequestJson () {
+    func alamofireRequestJson (completionHandler: @escaping (_ result: Any?, _ error: Any?) -> ()) {
         let user = "user"
         let password = "password"
         let credential = URLCredential(user: user, password: password, persistence: .forSession)
 
-        AF.request("https://api.github.com/search/users?q=andre")
+        AF.request("http://api.github.com/search/users?q=andre")
             .authenticate(with: credential)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
@@ -39,12 +59,15 @@ class ViewController: UIViewController {
             switch response.result {
             case .failure(let error):
                 print(error)
+                completionHandler(nil,error)
             case .success:
                 do {
                     let result = try JSONDecoder().decode(GitHubUser.self, from: response.data!)
                     print(result.total_count)
+                    completionHandler(result, nil)
                 } catch (let err) {
                     print(err.localizedDescription)
+                    completionHandler(nil, err)
                 }
                 
             }
